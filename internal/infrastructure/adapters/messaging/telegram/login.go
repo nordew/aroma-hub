5e2a164a1ec1efeea8b5737332f3a59eda460db9
r.horishnyi <horishnyi@proton.me>
2025-05-06 20:56:39 +0300

    -

--- internal/infrastructure/adapters/messaging/telegram/login.go
+++ internal/infrastructure/adapters/messaging/telegram/login.go
@@ -1,8 +1,6 @@
 package telegram
 
 import (
-	"aroma-hub/internal/application/dto"
-	"context"
 	"fmt"
 	"strconv"
 	"time"
@@ -15,18 +13,11 @@ func (p *TelegramProvider) registerLoginCommand() {
 }
 
 func (p *TelegramProvider) handleLogin(c telebot.Context) error {
-	ctx := context.Background()
-
 	userID := c.Sender().ID
 	vendorID := strconv.FormatInt(userID, 10)
 
-	admins, err := p.storage.ListAdmins(ctx, dto.ListAdminFilter{VendorID: vendorID})
-	if err != nil {
-		return c.Send(fmt.Sprintf("Error checking admin status: %v", err))
-	}
-
-	if len(admins) == 0 {
-		return c.Send("❌ You are not registered as an admin.")
+	if ok := p.isAdmin(userID); !ok {
+		return c.Send("You are not authorized to login.")
 	}
 
 	otp, err := p.otpGen.GenerateOTP(vendorID)
